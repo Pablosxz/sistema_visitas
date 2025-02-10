6f8d4d960ec035d6a1b24c3b01eba060692d6ae9
Pablo <lucasrn03@gmail.com>
2025-02-10 15:35:57 -0300

    feat: cria os controllers, models e views necessárias

---
+++ app/views/visits/_visit.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! visit, :id, :visitor_id, :sector_id, :employee_id, :visit_time, :unit_id, :created_at, :updated_at
+json.url visit_url(visit, format: :json)
---
+++ db/migrate/20250210183039_create_visitors.rb
@@ -0,0 +1,13 @@
+class CreateVisitors < ActiveRecord::Migration[8.0]
+  def change
+    create_table :visitors do |t|
+      t.string :cpf
+      t.string :name
+      t.string :rg
+      t.string :phone
+      t.string :photo
+
+      t.timestamps
+    end
+  end
+end
