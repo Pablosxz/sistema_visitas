7371d626d4a582c583880213b512fe221d1ff7eb
Pablo <lucasrn03@gmail.com>
2025-02-12 22:13:13 -0300

    feat: define as 'abilities' dos usuarios no model

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,32 +1,19 @@
-# frozen_string_literal: true
-
 class Ability
   include CanCan::Ability
 
   def initialize(user)
-    # Define abilities for the user here. For example:
-    #
-    #   return unless user.present?
-    #   can :read, :all
-    #   return unless user.admin?
-    #   can :manage, :all
-    #
-    # The first argument to `can` is the action you are giving the user
-    # permission to do.
-    # If you pass :manage it will apply to every action. Other common actions
-    # here are :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on.
-    # If you pass :all it will apply to every resource. Otherwise pass a Ruby
-    # class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the
-    # objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, published: true
-    #
-    # See the wiki for details:
-    # https://github.com/CanCanCommunity/cancancan/blob/develop/docs/define_check_abilities.md
+    user ||= User.new  # Caso não esteja logado
+
+    if user.admin?
+      can :manage, :all  # Administrador pode fazer tudo
+    elsif user.attendant?
+      can :read, Visitor
+      can :create, Visitor
+      can :manage, Visit, unit_id: user.unit_id  # Só pode gerenciar visitas da sua unidade
+    elsif user.employee?
+      employee = user.employee  # Pegamos o funcionário associado ao usuário
+      can :read, Visit, employee_id: employee.id
+      can :confirm, Visit, employee_id: employee.id  # Apenas confirma suas visitas
+    end
   end
 end
