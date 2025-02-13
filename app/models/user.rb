a24b32808bf1060425c4dde8556ae01bb800ed59
Pablo <lucasrn03@gmail.com>
2025-02-12 22:14:21 -0300

    fix: ajusta relação entre 'user' e 'funcionario'

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  has_many :employees, dependent: :destroy, foreign_key: "user_id"
+  has_one :employee, dependent: :destroy  # Um usuário tem um único funcionário
 
   enum :role, { admin: 0, attendant: 1, employee: 2 } # Enum para definir os papéis dos usuários
 end
