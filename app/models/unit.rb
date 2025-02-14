8bee919ca08dcc82cced575ea7db8dade3b6e175
Pablo <lucasrn03@gmail.com>
2025-02-14 13:20:41 -0300

    feat: implementa desativação automática de atendentes ao desativar unidade

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,13 +1,14 @@
 class Unit < ApplicationRecord
     has_many :visits
     has_many :sectors
+    has_many :users
 
     scope :active, -> { where(active: true) }
   
     validates :name, presence: true
 
     # Callback para desativar os setores quando a unidade for desativada
-    before_update :deactivate_sectors_if_inactive, if: :inactive?
+    before_update :deactivate_sectors_and_attendants_if_inactive, if: :inactive?
 
     def inactive?
         !self.active
@@ -22,12 +23,20 @@ class Unit < ApplicationRecord
     end
 
     # Método para desativar os setores associados junto com a unidade
-    def deactivate_sectors_if_inactive
+    def deactivate_sectors_and_attendants_if_inactive
         if self.sectors.present?
             ActiveRecord::Base.transaction do
                 sectors = self.sectors
                 sectors.each do |sector|
                     sector.deactivate!
+                end
+            end
+        end
+        if self.users.present?
+            ActiveRecord::Base.transaction do
+                users = self.users
+                users.each do |user|
+                    user.destroy!
                 end
             end
         end
