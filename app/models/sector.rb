5e39e785b0a94890fd5816b447c25d206920a3d0
Pablo <lucasrn03@gmail.com>
2025-02-14 13:06:13 -0300

    feat: implementa desativação automática de funcionarios ao desativar setor

--- app/models/sector.rb
+++ app/models/sector.rb
@@ -8,11 +8,29 @@ class Sector < ApplicationRecord
   validates :name, presence: true
   validates :unit_id, presence: true
 
+  # callback para desativar os funcionários quando o setor for desativado
+  before_update :deactivate_employees_if_inactive, if: :inactive?
+
+  def inactive?
+    !self.active
+  end
+
   def deactivate!
     update(active: false)
   end
 
   def activate!
     update(active: true)
+  end
+
+  def deactivate_employees_if_inactive
+    if self.employees.present?
+      ActiveRecord::Base.transaction do
+        employees = self.employees
+        employees.each do |employee|
+          employee.deactivate!
+        end
+      end
+    end
   end
 end
